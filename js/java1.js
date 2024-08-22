f99e257a0a491b68bde1523c82c2092b58f8e89d
[juan roman] <[juancanas651@gmail.com]>
2024-08-21 20:01:58 -0500

    final

--- js/java1.js
+++ js/java1.js
@@ -4,36 +4,24 @@ function abrircaja(imageUrl) {
     document.getElementById('caja').style.display = 'flex';
 }
 
-function cerrarcaja(event) {
+function cerrarcaja(evento) {
     const caja = document.getElementById('caja');
     const image = document.getElementById('imagen');
 
 
-    if (event.target !== image) {
+    if (evento.target !== image) {
         caja.style.display = 'none';
 
     }
 }
 
-// Añadir el evento de scroll para hacer zoom
-const image = document.getElementById('imagen');
-let scale = 1;
+window.onload = function() {
 
-image.addEventListener('wheel', function(event) {
-    event.preventDefault(); // Evitar que la página se desplace al hacer zoom
+    document.getElementById('promo').style.display = 'flex';
 
-    // Definir el factor de zoom
-    const zoomFactor = 0.1;
+    document.getElementById('cerrarpromo').onclick = function() {
+        document.getElementById('promo').style.display = 'none';
+    };
+};
 
-    // Ajustar la escala según el scroll
-    if (event.deltaY < 0) {
-        // Scroll hacia arriba (acercar)
-        scale += zoomFactor;
-    } else {
-        // Scroll hacia abajo (alejar)
-        scale = Math.max(1, scale - zoomFactor); // Evitar que la escala sea menor a 1
-    }
 
-    // Aplicar la transformación de escala
-    image.style.transform = `scale(${scale})`;
-});
